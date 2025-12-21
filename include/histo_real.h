f523105c12fc6ec73a87a7ff32cfaf3fad60dbec
Edmanito <168591861+Edmanito@users.noreply.github.com>
2025-12-21 22:07:49 +0100

    push vide fixe bug

--- include/histo_real.h
+++
@@ -1,6 +0,0 @@
-#ifndef HISTO_REAL_H
-#define HISTO_REAL_H
-
-int calculer_histo_real(const char *chemin_entree, const char *chemin_sortie);
-
-#endif
--- include/histo_src.h
+++
@@ -1,6 +0,0 @@
-#ifndef HISTO_SRC_H
-#define HISTO_SRC_H
-
-int generer_histo_src(const char *chemin_entree, const char *chemin_sortie);
-
-#endif
